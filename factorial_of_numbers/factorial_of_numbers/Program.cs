be2d76cac3f4e528cc368bd08bac7f13e9bd4123
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-03-21 13:38:26 +0100

    Program.cs added

--- factorial_of_numbers/factorial_of_numbers/Program.cs
+++ factorial_of_numbers/factorial_of_numbers/Program.cs
@@ -1,7 +1,14 @@
-﻿class Program
+﻿class Programm
 {
     static void Main()
     {
-
+        Console.WriteLine("enter number:");
+        int input = Convert.ToInt32(Console.ReadLine());
+        int result=1;
+        for (int i = 0; i <= input; i++)
+        {
+            result *= i;
+        }
+        Console.WriteLine($"resultat: {result}");
     }
 }
